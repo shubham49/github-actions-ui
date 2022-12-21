be6ba34a03e9f39bacb77d05c9ba2faa3e9b6597
shubham.garg <shubham.garg@thoughtworks.com>
2022-12-21 17:30:13 +0530

    initial commit

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,28 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { User, UserAuthResp, Authority, Role } from '../models/User';
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  login(user: User) {
-    const body = JSON.stringify(user);
-    const headers: HttpHeaders = new HttpHeaders({ 'content-Type': 'application/json' });
-
-    this.http.post<UserAuthResp>(environment.serverUrl + '/user/login', body, { headers })
-      .subscribe(data => {
-        sessionStorage.setItem('loggedUser',
-          JSON.stringify({
-            user: data.user,
-            token: data.token
-          }));
-        window.location.reload();
-      });
-
-  }
   saveToken(pac: string) {
     localStorage.setItem("github-pac", pac);
   }
